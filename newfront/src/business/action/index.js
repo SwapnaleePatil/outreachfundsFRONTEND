55ea6e8ae426ad1801e730f215ca5e47a4644f5b
mehulbhanderi <lanetteam.mehul32@gmail.com>
2018-03-14 19:38:04 +0530

    student and business login with validation and carosoul and business listing

--- newfront/src/business/action/index.js
+++ newfront/src/business/action/index.js
@@ -2,18 +2,17 @@ import axios from 'axios';
 
 export const businessSignup=(page=1)=>{
         return {
-
         type:'BUSINESS_SIGNUP',
         payload:page
     }
-}
+};
 export const businessFields=(obj=[])=>{
 
     return {
         type:'BUSINESS_FIELDS',
         payload:obj
     }
-}
+};
 export const addBusiness=(obj)=>{
     return (dispatch)=>{
         axios.post("http://localhost:3000/api/business/profile",obj).then((result)=>{
@@ -22,6 +21,19 @@ export const addBusiness=(obj)=>{
                 type:'BUSINESS_ADD',
                 payload:result.data
             })
+            localStorage.setItem('user',result.data.record);
         })
     }
-}
+};
+export const listBusiness=()=>{
+    return(dispatch)=>{
+        axios.get("http://localhost:3000/api/business/profile").then((result)=>{
+            dispatch({
+                type:'BUSINESS_LIST',
+                payload:result.data.record
+            });
+        }).catch((e)=>{
+            console.log(e);
+        })
+    }
+};
